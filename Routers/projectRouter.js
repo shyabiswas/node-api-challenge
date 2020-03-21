47a6dbeaeff0da9cea8a049315ce5b1aab6fa2ed
Shya Biswas <shyabiswas@gmail.com>
2020-03-21 00:09:39 -0400

    cont

--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -7,26 +7,6 @@ const router = express.Router();
 
 router.use(express.json());
 
-// custom middleware
-function validateProjectId(req, res, next) {
-    if (req.params.id) {
-        req.project = req.params.id;
-        next();
-    } else {
-        res.status(400).json({message: 'Invalid project id'})
-    }
-}
-
-function validateProject(req, res, next) {
-    if (!req.body.name || !req.body.description) {
-        res.status(400).json({message: 'Provide project name and description'});
-    } else {
-        next();
-    }
-}
-
-
-
 // get all projects
 router.get('/', (req, res) => {
     Projects.get()
@@ -93,5 +73,23 @@ router.delete('/:id', validateProjectId, (req, res) => {
         res.status(500).json({error: 'Unable to delete project'})
     })
 })
+// custom middleware
+function validateProjectId(req, res, next) {
+    if (req.params.id) {
+        req.project = req.params.id;
+        next();
+    } else {
+        res.status(400).json({message: 'Invalid project id'})
+    }
+}
 
-module.exports = router, validateProject, validateProjectId;
+function validateProject(req, res, next) {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({message: 'Provide project name and description'});
+    } else {
+        next();
+    }
+}
+
+
+module.exports = router;
